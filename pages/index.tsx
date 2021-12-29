3aa82855555f0e26c2e57a0e7f5aa3e233dcc02c
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2021-12-29 10:09:27 +0100

    Pre deploy to Netlify

---
+++ pages/index.tsx
@@ -0,0 +1,101 @@
+// Next Components
+import { InferGetStaticPropsType } from 'next';
+import Head from 'next/head';
+
+// Styles
+import styled from '@emotion/styled';
+
+// Styles with StyledComponents
+const Container = styled.div`
+    min-height: 100vh;
+    padding: 0 0.5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Main = styled.main`
+    padding: 5rem 0;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const BlogTitle = styled.h1`
+    margin: 0;
+    line-height: 1.15;
+    font-size: 4rem;
+`;
+
+const List = styled.ul`
+    list-style-type: none;
+`;
+
+const ListItem = styled.li`
+    padding: 10px;
+    text-transform: capitalize;
+    margin: 40px 0;
+    cursor : pointer;
+    color #252525;
+    &:hover{
+        background: #f0f0f0
+    }
+`;
+
+const PostTitle = styled.h2`
+    margin: 0;
+    font-size: 24px;
+`;
+
+const PostBody = styled.p`
+    font-size: 16px;
+`;
+
+const title: string = 'Next.js + TypeScript';
+
+export default function Home({
+    posts,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+    return (
+        <Container>
+            <Head>
+                <title>My awesome blog</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Main>
+                <BlogTitle>{title}</BlogTitle>
+                <List>
+                    {posts.map((post) => (
+                        <ListItem key={post.id}>
+                            <PostTitle>{post.title}</PostTitle>
+                            <PostBody>{post.body}</PostBody>
+                        </ListItem>
+                    ))}
+                </List>
+            </Main>
+        </Container>
+    );
+}
+
+export type Post = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+};
+
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+    const posts: Post[] = await res.json();
+
+    return {
+        props: {
+            posts,
+        },
+    };
+};
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,73 @@
+// Next components
+import {
+    GetStaticPropsContext,
+    InferGetStaticPropsType,
+    GetStaticPaths,
+} from 'next';
+
+// Custom Components
+import { Article } from '@components/Article';
+
+// Custom Types
+import type { Post } from '../index';
+
+export default function BlogPost({
+    post,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+    return (
+        <Article>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+        </Article>
+    );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+    const posts: Post[] = await res.json();
+
+    const paths = posts.map((post) => ({
+        params: {
+            id: post.id.toString(),
+        },
+    }));
+
+    return {
+        paths,
+        fallback: false,
+    };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+    const { params } = context;
+    // Now we can get the posts/1 thanks to the params.id coming from context
+
+    // * The params object can be undefined, so we will take care about this
+    const emptyPost: Post = {
+        title: 'Post not found',
+        body: '',
+        id: 0,
+        userId: 0,
+    };
+
+    if (!params?.id) {
+        return {
+            props: {
+                post: emptyPost,
+            },
+        };
+    }
+
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${params.id}`
+    );
+
+    const post: Post = await res.json();
+
+    return {
+        props: {
+            post,
+        },
+    };
+};
